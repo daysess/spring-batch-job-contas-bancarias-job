132efbd820bf0add0753b0f374c71ea3ce1af0e2
Dayse Soares <daysedesign@gmail.com>
2020-07-03 14:22:41 -0300

    Novo projeto

---
+++ src/main/java/br/com/daysesoares/contasbancariasjob/ContasBancariasJobApplication.java
@@ -0,0 +1,13 @@
+package br.com.daysesoares.contasbancariasjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasBancariasJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasBancariasJobApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/daysesoares/contasbancariasjob/dominio/Cliente.java
@@ -0,0 +1,18 @@
+package br.com.daysesoares.contasbancariasjob.dominio;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class Cliente {
+
+	private String nome;
+	
+	private Integer idade;
+	
+	private String email;
+	
+	private Double faixaSalarial;
+	
+	
+}
